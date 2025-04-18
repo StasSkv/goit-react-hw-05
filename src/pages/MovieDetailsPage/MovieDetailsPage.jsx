bed45f348e5af7639b2409f51bd5056428ea0b53
StasSkv <stas000123@gmail.com>
2025-04-19 01:45:36 +0300

    debug-url

--- src/pages/MovieDetailsPage/MovieDetailsPage.jsx
+++ src/pages/MovieDetailsPage/MovieDetailsPage.jsx
@@ -1,40 +1,66 @@
-import { useLocation, useNavigate, NavLink, Outlet } from 'react-router-dom';
+import { useLocation, useNavigate, NavLink, Outlet, useParams } from 'react-router-dom';
 import css from './MovieDetailsPage.module.css';
 import clsx from 'clsx';
-import { Suspense, useRef, useEffect } from 'react';
+import { Suspense, useRef, useState, useEffect } from 'react';
 import Loader from '../../components/Loader/Loader';
+import { fetchMovieById } from '../../apiService';
 
 const MovieDetailsPage = () => {
+  const { movieId } = useParams();
   const location = useLocation();
   const navigate = useNavigate();
   const outletRef = useRef(null);
-  const movie = location.state?.movie;
+  const [movie, setMovie] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
   const from = location.state?.from || '/';
-  const movieId = movie?.id;
+
+  useEffect(() => {
+    const fetchMovieDetails = async () => {
+      if (!movieId) {
+        setError('Фільм не знайдений');
+        setLoading(false);
+        return;
+      }
+
+      try {
+        const data = await fetchMovieById(movieId);
+        setMovie(data);
+      } catch (error) {
+        setError(error.message);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchMovieDetails();
+  }, [movieId]);
+
+  if (loading) {
+    return <Loader />;
+  }
+
+  if (error) {
+    setError(error.message);
+  }
 
   const activeLinkClass = ({ isActive }) => {
     return clsx(css.link, isActive && css.active);
   };
-
-useEffect(() => {
-  setTimeout(() => {
-    if (outletRef.current) {
-      outletRef.current.scrollIntoView({ behavior: 'smooth' });
-    }
-  }, 500);
-}, [location.pathname]);
 
   return (
     <div className={css.movieDetailsWrap}>
       <button onClick={() => navigate(from)}>Go back</button>
 
       <div className={css.movieDetails}>
-        <img
-          src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
-          width={300}
-          height={300}
-          alt={movie.original_title}
-        />
+        {movie?.poster_path && (
+          <img
+            src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
+            width={300}
+            height={300}
+            alt={movie.original_title}
+          />
+        )}
         <div className={css.description}>
           <h2>{movie.original_title}</h2>
           <p>{movie.overview}</p>
